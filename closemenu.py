f32c9d73d724ab2cc0b0f9552681e1b39de85236
Raghavapriyan Saravanapriyan <s.raghavapriyan@gmail.com>
2025-10-22 23:48:02 +0000

    fixed circular import issue

--- closemenu.py
+++ closemenu.py
@@ -1,4 +1,3 @@
-import main
 def exit_request():
     exit_choice = input("Do you want to exit the program? (y/n): ").lower()
     if exit_choice == 'y':
@@ -7,6 +6,7 @@ def exit_request():
     elif exit_choice == 'n':
         print("Returning to the main menu.\n")
         print("-----------------------------------------------------------------------------------------------------------------------\n")
+        import main
         main.main_menu()
     else:
         print("Invalid input. Please enter 'y' or 'n'.\n")
