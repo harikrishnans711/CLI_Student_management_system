739f6f4588d259dbc5e4da3e3fa805b6b5c95e76
Raghavapriyan Saravanapriyan <s.raghavapriyan@gmail.com>
2025-10-22 22:56:38 +0000

    main menu design ver1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+#imports
 from banner import banner_print
+from main_menu import main_menu
+
+#banner
 banner_print()
+
+#main menu
+main_menu()
+
+
---
+++ main_menu.py
@@ -0,0 +1,9 @@
+def main_menu():
+    print("Welcome to CLI Student management system!\n")
+    print("Choose one of the following options:\n")
+    print("[1] View list of all existing student profiles")
+    print("[2] Create new student profile")
+    print("[3] Browse particular profile")
+    print("[4] Edit existing profile")
+    print("[5] Delete existing profile\n")
+    choice=int(input("Your option - "))
