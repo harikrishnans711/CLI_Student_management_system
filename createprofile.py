f3dbb654da5714d685d0da35ea9fb4a71580224a
raghav <s.raghavapriyan@gmail>
2025-10-24 11:18:03 +0530

    fixed reg no duplication

--- createprofile.py
+++ createprofile.py
@@ -1,10 +1,16 @@
-import view_all
+from view_all import data
 from closemenu import exit_request
 
 def create_profile():
+    print("Creating a new student profile...\n")
     try:
-        print("Creating a new student profile...\n")
         reg_no = int(input("Enter Registration Number[integer only]: "))
+        regnos = list(i[0] for i in data)
+        if reg_no in regnos:
+            print("Registration Number already exists! TRY AGAIN...")
+            create_profile()
+        else:
+            pass
         name = input("Enter Name: ")
         grade = input("Enter Grade: ")
         section = input("Enter Section: ")
@@ -12,8 +18,9 @@ def create_profile():
         email = input("Enter Email Address: ")
         test_scores = input("Enter Test Scores (comma separated): ")
         new_profile = [reg_no, name, grade, section, phone, email, test_scores]
-        view_all.data.append(new_profile)
-        print("\nNew student profile created successfully!\n")
+        data.append(new_profile)
+        print("Profile created successfully!")
     except:
         print("Invalid Input")
     exit_request()
+create_profile()
