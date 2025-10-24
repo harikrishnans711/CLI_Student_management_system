ad6becedcb0cf264bdc0b07c045ce5c60f24add2
raghav <s.raghavapriyan@gmail>
2025-10-24 10:44:36 +0530

    fixed raised issue regarding data type mismatch

--- createprofile.py
+++ createprofile.py
@@ -2,17 +2,19 @@ import view_all
 from closemenu import exit_request
 
 def create_profile():
-    print("Creating a new student profile...\n")
-    reg_no = int(input("Enter Registration Number[integer only]: "))
-    name = input("Enter Name: ")
-    grade = input("Enter Grade: ")
-    section = input("Enter Section: ")
-    phone = input("Enter Phone Number: ")
-    email = input("Enter Email Address: ")
-    test_scores = input("Enter Test Scores (comma separated): ")
-
-    new_profile= [reg_no, name, grade, section, phone, email, test_scores]
-    view_all.data.append(new_profile)
-
-    print("\nNew student profile created successfully!\n")
+    try:
+        print("Creating a new student profile...\n")
+        reg_no = int(input("Enter Registration Number[integer only]: "))
+        name = input("Enter Name: ")
+        grade = input("Enter Grade: ")
+        section = input("Enter Section: ")
+        phone = input("Enter Phone Number: ")
+        email = input("Enter Email Address: ")
+        test_scores = input("Enter Test Scores (comma separated): ")
+        new_profile = [reg_no, name, grade, section, phone, email, test_scores]
+        view_all.data.append(new_profile)
+        print("\nNew student profile created successfully!\n")
+    except:
+        print("Invalid Input")
     exit_request()
+create_profile()
